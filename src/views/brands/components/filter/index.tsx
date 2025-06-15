ef62eec565da328c9e1fb184a693d892b4c13d5f
Mohamed Sameh <mxsameh@gmail.com>
2025-06-15 19:11:31 +0300

    sync

--- src/views/brands/components/filter/index.tsx
+++ src/views/brands/components/filter/index.tsx
@@ -13,11 +13,11 @@ export const Filter = (props: Iprops) => {
         <li className={!category ? "active" : ""}>
           <Link to="/brands">ALL</Link>
         </li>
-        <li className={category == "menswear" ? "active" : ""}>
-          <Link to="/brands?category=menswear">MENSWEAR</Link>
-        </li>
         <li className={category == "womenswear" ? "active" : ""}>
           <Link to="/brands?category=womenswear">WOMENSWEAR</Link>
+        </li>
+        <li className={category == "menswear" ? "active" : ""}>
+          <Link to="/brands?category=menswear">MENSWEAR</Link>
         </li>
         <li className={category == "accessories" ? "active" : ""}>
           <Link to="/brands?category=accessories">ACCESSORIES</Link>
