00f0e49637883c51049583cbee7e8af46353997c
maison-pyramide-co <137396278+maison-pyramide-co@users.noreply.github.com>
2026-06-19 16:42:45 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import svgr from "vite-plugin-svgr";
 import autoprefixer from "autoprefixer";
 import postcssPresetEnv from "postcss-preset-env";
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [svgr(), react()],
